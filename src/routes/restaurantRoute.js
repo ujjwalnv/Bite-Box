46aea1c98b0c9060cda9a35e1a12459c6a67f07b
Ujjwal Verma <vujjwal1n@gmail.com>
2024-02-04 04:38:31 +0530

    Added TODOs for restaurant route

--- src/routes/restaurantRoute.js
+++ src/routes/restaurantRoute.js
@@ -4,12 +4,14 @@ import { Restaurant } from '../models/restaurantModel.js';
 
 const router = express.Router();
 
-
+// TODO: Create and add items Restaurants based on their id
 
 router.get('/restaurant', async (req, res) => {
     const result = await Restaurant.find({});
     
     res.send({data: result});
 })
+
+// TODO: Get restaurant by id
 
 export default router;
