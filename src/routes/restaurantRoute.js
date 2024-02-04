5e7cb51e479ee1d0d4917350dda3281ae8c7ea47
Ujjwal Verma <vujjwal1n@gmail.com>
2024-02-04 05:38:43 +0530

    Created POST /addItems and GET /restaurant/:id routes

--- src/routes/restaurantRoute.js
+++ src/routes/restaurantRoute.js
@@ -1,10 +1,36 @@
 import { constants } from 'http2';
 import express from 'express';
-import { Restaurant } from '../models/restaurantModel.js';
+import { Item, Restaurant } from '../models/restaurantModel.js';
 
 const router = express.Router();
 
-// TODO: Create and add items Restaurants based on their id
+// Create and add items Restaurants based on their id
+router.post('/addItems', async (req, res) => {
+    // Get restaurant
+    const restaurant_id = req.body.id;
+    const items = req.body.items;
+    let counter = 0;
+    
+    // iterate through items array, Create item for each and add that item to the restaurant model
+    for(const element of items) {
+        const item_name = element.name;
+        const item_price = element.price;
+
+        const item = await Item.create({
+            name: item_name,
+            price: item_price
+        })
+
+        await Restaurant.findByIdAndUpdate(
+            restaurant_id,
+            {$push: {items: item}}
+        )
+        counter++;
+    }
+
+    
+    res.send({message: `Added ${counter} items to the menu.`});
+})
 
 router.get('/restaurant', async (req, res) => {
     const result = await Restaurant.find({});
@@ -12,6 +38,16 @@ router.get('/restaurant', async (req, res) => {
     res.send({data: result});
 })
 
-// TODO: Get restaurant by id
+// Get restaurant by id
+router.get('/restaurant/:id', async (req, res) => {
+    const id = req.params.id;
+    const restaurant = await Restaurant.findById(id);
+
+    if(!restaurant){
+        return res.status(404).send({message: "Book not found!"})
+    }
+
+    return res.status(200).send(restaurant)
+})
 
 export default router;
